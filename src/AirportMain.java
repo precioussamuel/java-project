b47c8b34626efa6172b7509da4bfe5deddb03cfc
precious samuel <precious20samuel@gmail.com>
2025-09-25 15:52:31 +0100

    airport main

--- src/AirportMain.java
+++ src/AirportMain.java
@@ -1,7 +1,7 @@
 public class AirportMain {
     public static void main(String[] args) {
         Airport airport = new Airport("Serverless Deadline", "Lagos Ikeja", 50, 409479);
-        System.out.println("the aiport name is " + airport.getAirportName());
+        System.out.println("the airport name is " + airport.getAirportName());
         System.out.println("the airport address is " + airport.getAirportAddress());
         System.out.println("the airport staff numbers is " + airport.getAirportStaffNumber());
         System.out.println("the airport ID is " + airport.getAirportID());
